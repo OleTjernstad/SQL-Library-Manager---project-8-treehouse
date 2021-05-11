ffe0e54b763a90aff36e3ed970ce044278a21a6f
Ole Tjernstad <ott556@gmail.com>
2021-05-11 14:23:23 +0200

    add new book

--- routes/index.js
+++ routes/index.js
@@ -3,46 +3,83 @@ const router = express.Router();
 
 const { Book } = require('../models');
 
+const asyncHandler = (cb) => {
+    return async (req, res, next) => {
+        try {
+            await cb(req, res, next);
+        } catch (error) {
+            // Forward error to the global error handler
+            next(error);
+        }
+    };
+};
+
 /* GET home page. */
-router.get('/', async (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.redirect('/books');
 });
 
-router.get('/books', async (req, res, next) => {
-    // Shows the full list of books
-    const books = await Book.findAll();
-    res.render('index', { books });
+router.get(
+    '/books',
+    asyncHandler(async (req, res, next) => {
+        // Shows the full list of books
+        const books = await Book.findAll();
+        res.render('index', { books });
+    })
+);
+
+router.get('/books/new', (req, res, next) => {
+    res.render('new-book');
 });
 
-router.get('/books/new', async (req, res, next) => {
-    // Shows the create new book form
-    const books = await Book.findAll();
-    res.json(books);
-});
+router.post(
+    '/books/new',
+    asyncHandler(async (req, res, next) => {
+        let book;
+        try {
+            book = await Book.create(req.body);
+            res.redirect('/books');
+        } catch (error) {
+            if (error.name === 'SequelizeValidationError') {
+                book = await Book.build(req.body);
+                res.render('new-book', {
+                    article,
+                    errors: error.errors,
+                    title: 'New Book'
+                });
+            } else {
+                throw error;
+            }
+        }
+    })
+);
 
-router.post('/books/new', async (req, res, next) => {
-    // Posts a new book to the database
-    const books = await Book.findAll();
-    res.json(books);
-});
+router.get(
+    '/books/:id',
+    asyncHandler(async (req, res, next) => {
+        // Shows book detail form
+        const book = await Book.findByPk(req.params.id);
+        res.render('book', { book });
+    })
+);
 
-router.get('/books/:id', async (req, res, next) => {
-    // Shows book detail form
-    const books = await Book.findAll();
-    res.json(books);
-});
+router.post(
+    '/books/:id',
+    asyncHandler(async (req, res, next) => {
+        // Updates book info in the database
+        const books = await Book.findAll();
+        res.json(books);
+    })
+);
 
-router.post('/books/:id', async (req, res, next) => {
-    // Updates book info in the database
-    const books = await Book.findAll();
-    res.json(books);
-});
+router.post(
+    '/books/:id/delete',
+    asyncHandler(async (req, res, next) => {
+        // Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting
 
-router.post('/books/:id/delete', async (req, res, next) => {
-    // Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting
-
-    const books = await Book.findAll();
-    res.json(books);
-});
+        const books = await Book.findAll();
+        res.json(books);
+    })
+);
 
 module.exports = router;
