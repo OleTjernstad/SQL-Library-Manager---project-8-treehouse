c2037e8572eea9b0860cc5b76bf47ca9c326087f
Ole Tjernstad <ott556@gmail.com>
2021-05-11 10:53:33 +0200

    add styling and error handler

--- models/book.js
+++ models/book.js
@@ -1,43 +1,43 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Book extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Book extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  Book.init(
-    {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: "Please provide a title for the book",
-          },
+    Book.init(
+        {
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'Please provide a title for the book'
+                    }
+                }
+            },
+            author: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'Please provide a author for the book'
+                    }
+                }
+            },
+            genre: DataTypes.STRING,
+            year: DataTypes.INTEGER
         },
-      },
-      author: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: "Please provide a author for the book",
-          },
-        },
-      },
-      genre: DataTypes.STRING,
-      year: DataTypes.INTEGER,
-    },
-    {
-      sequelize,
-      modelName: "Book",
-    }
-  );
-  return Book;
+        {
+            sequelize,
+            modelName: 'Book'
+        }
+    );
+    return Book;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const { Book } = require("../models");
+const { Book } = require('../models');
 
 /* GET home page. */
-router.get("/", async (req, res, next) => {
-  const books = await Book.findAll();
-  res.json(books);
+router.get('/', async (req, res, next) => {
+    const books = await Book.findAll();
+    res.json(books);
 });
 
 module.exports = router;
